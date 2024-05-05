e3bf871fb1104cc625390836bcddd9a178f84af4
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-05 18:28:45 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "45bvda"
+// let score = null
+// let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1;  false => 0
+
+let isLoggedIn = "payal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "payal" => true
+
+let someNumber = 66
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
