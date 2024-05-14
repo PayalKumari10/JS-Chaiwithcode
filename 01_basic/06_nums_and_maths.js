b8f34f9230a7d1a9ed3c93c39b3eb9e16ee09b3a
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-14 18:27:38 +0000

    add some nums

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -5,3 +5,8 @@ const balance = new Number(100)
 console.log(balance);
 
 console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));
