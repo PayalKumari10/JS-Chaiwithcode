438fdf75a7e34ab2bfe58c4587b881f503aaf362
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-17 08:16:37 +0000

    01_objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,39 @@
+// singleton (when we can decleare contract then create singleton)
+//object.create 
+
+//object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Payal",
+    "full name": "Payal Kumari",
+    age: 18,
+    mySym: "mykey1",
+    location: "Ara",
+    email: "payal@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Friday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]);
+
+JsUser.email = "payal@chatgpt.com"
+// Object.freeze(JsUser);
+JsUser.email = "payal@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
