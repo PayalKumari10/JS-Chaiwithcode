c272c650db64ee4346d2c2ab418af47c1e74f8d0
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-19 09:38:31 +0000

    04_objects

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,55 @@
+//const linkedinUser = new Object()
+const linkedinUser = {}
+
+linkedinUser.id = "123abc"
+linkedinUser.name = "ram"
+linkedinUser.isLoggedIn = false
+
+//console.log(linkedinUser);
+
+const regularUser = {
+    email: "kumar@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "payal",
+            lastname: "kumari"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2 }
+//const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "payal@gmail.com"
+    },
+    {
+        id: 4,
+        email: "payal@gmail.com"
+    },
+    {
+        id: 2,
+        email: "payal@gmail.com"
+    }
+]
+
+users[1].email
+console.log(linkedinUser);
+
+console.log(Object.keys(linkedinUser));
+console.log(Object.values(linkedinUser));
+console.log(Object.entries(linkedinUser));
+
+console.log(linkedinUser.hasOwnProperty('isLoggedIn'));
+console.log(linkedinUser.hasOwnProperty('isLogged'));
