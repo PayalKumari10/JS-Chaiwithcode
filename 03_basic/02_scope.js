99f69e50cd8cd788deda47966772723046a6517c
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-24 06:40:54 +0000

    02_scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,47 @@
+//var c = 300
+let a = 300
+if(true) {
+    let  = 10
+    const b = 20
+    console.log("INNER",a);
+}
+
+// console.log(a);
+
+function one(){
+    const username = "payal"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+if (true) {
+    const username = "payal"
+    if (username === "payal") {
+        const website = " youtube "
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ intersting ++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1;
+}
+
+console.log(addone(5));
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
