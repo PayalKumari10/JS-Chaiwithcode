6526b17c1d51a846765f4d64ff640a9aaf26f1b9
PAYAL KUMARI <123013327+PayalKumari10@users.noreply.github.com>
2024-05-25 09:01:29 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();     //here we can use semicolon (;)
+
+(function aurcode(){
+    
+    console.log(    `DB CONNECTED TWO`);
+} )();
+
+((name) => {
+    console.log(    `DB CONNECTED THREE ${name}`);
+} )('payal')    //iffe use for remove the pollutions in global scope.
